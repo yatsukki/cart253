40fee6a14ffca5a1739e1df037120926281de486
yatsukki <kervenc35@gmail.com>
2025-09-25 00:30:58 -0400

    Adding facial features
    added eyes, eyebrows, mustache

--- art-jam/js/script.js
+++ art-jam/js/script.js
@@ -11,8 +11,53 @@
  * creating my canvas  
 */
 function setup() {
-    createCanvas ( 900, 700);
+    createCanvas (900, 700);
     background (240)
+}
+
+function drawEyes(offsetX, browX = 340) {
+//creating parameter for moving the eyes
+push();
+translate(offsetX, 0);
+    //drawing the eyes
+push ();
+noStroke();
+fill("#ffffffff");
+ellipse(390, 370, 87, 87);
+pop();
+//pupils
+push ();
+noStroke();
+fill("#000000ff");
+ellipse(390, 370, 63, );
+pop();
+//square skin for hiding pupils
+push ();
+noStroke();
+fill("#b47055ff")
+square(346, 285, 88)
+pop();
+//eyebrow
+push();
+fill("#000000ff");
+noStroke();
+rect(browX, 325, 100, 30);
+pop();
+    pop();
+   
+}
+
+function drawStache() {
+push ();
+strokeWeight(24);
+strokeCap(SQUARE);
+line(380, 450, 450, 445);
+line(448, 445, 520, 450);
+pop();
+//drawing pinch
+push ();
+
+pop();
 }
 
 
@@ -24,6 +69,11 @@ function draw() {
 push ();
 strokeWeight(12);
 fill("#b47055ff");
-ellipse(450, 350, 250, 320);
+ellipse(450, 350, 280, 320);
 pop();
+drawStache();
+drawEyes();
+drawEyes(120, 340); //drawing duplicate eyes 100 pixels to the right
+push ();
 }
+

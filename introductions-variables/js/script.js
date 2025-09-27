899f11cc86d6483a5f1c05bcecaa98d54a28e2a8
yatsukki <kervenc35@gmail.com>
2025-09-27 18:51:05 -0400

    finished intro to variables

--- introductions-variables/js/script.js
+++ introductions-variables/js/script.js
@@ -2,17 +2,16 @@
  * Title of Project
  * Author Name
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * learing what variables does
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(400, 500);
 }
 
 
@@ -20,5 +19,12 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(12);
+    //drawing circle
+    push;
+    noStroke();
+    fill(mouseX, mouseY, 0);
+    ellipse(width / 2, height / 2, mouseX, mouseY );
+    pop;
 
 }

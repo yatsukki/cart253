0c1fb0600f811d8512d41cbdd7ab3724be8d8722
yatsukki <kervenc35@gmail.com>
2025-09-28 06:02:11 -0400

    added a proper velocity model

--- topic/variables/movement/js/script.js
+++ topic/variables/movement/js/script.js
@@ -7,18 +7,51 @@
  */
 
 "use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+let bird={
+    x: 100,
+    y: 500,
+    size: 20,
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    minVelocity: {
+        x: -5,
+        y: -5
+    },
+    maxVelocity: {
+        x: 5,
+        y: 5
+    },
+    acceleration: {
+        x: 0.2,
+        y: -1
+    }
+}/**
+ 
 */
 function setup() {
-
+ createCanvas(500, 500);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(10);
+
+    //draw bird
+    push();
+    fill(255, 255, 0);
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
+    //move bird
+bird.velocity.x= bird.velocity.x + bird.acceleration.x;
+bird.velocity.y= bird.velocity.y + bird.acceleration.y; 
+
+//limit velocity
+bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x= bird.x + bird.velocity.x;
+    bird.y= bird.y + bird.velocity.y;
 
 }

14fc75d81c97b4ca71d2a7832b2ccbbf0c5968c6
yatsukki <kervenc35@gmail.com>
2025-09-29 23:55:59 -0400

    Finished tutorial on time

--- topic/events/js/script.js
+++ topic/events/js/script.js
@@ -1,24 +1,22 @@
-/**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
-
-"use strict";
-
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
-
+const ball = {
+    x: 0,
+    y: 250,
+    size: 50,
+    fill: "#ffffff"
 }
 
+function setup() {
+    createCanvas (500, 500);
+}
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+function draw () {
+    background(0);
 
+    ball.x += 1;
+    
+    push();
+    noStroke;
+    fill(ball.fill);
+    ellipse(ball.x, ball.y. ball.size);
+    pop();
 }

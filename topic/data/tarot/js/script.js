3c1dce85bd31f18f6bbd2dd61533d9cc2f020e18
yatsukki <kervenc35@gmail.com>
2025-11-11 21:11:29 -0500

    finished tarot project

--- topic/data/tarot/js/script.js
+++ topic/data/tarot/js/script.js
@@ -7,17 +7,53 @@
 
 "use strict";
 
+
+//our tarot data
+
+let tarot = undefined //undefined because it's not even loaded yet
+
+//our fortune
+let fortune = "Click to show a fortune"
+
+function preload(){
+    tarot = loadJSON ('assets/data/tarot_interpretations.json')
+    
+}
+
 /**
- * tbd.
+ * create a canvas
 */
 function setup() {
+    createCanvas (1080, 400);
 
 }
 
 
 /**
- * tbd.
+ * display tarot
 */
 function draw() {
+    background(0);
 
+    const description = tarot.description;
+    const gullible = tarot.tarot_interpretations[0].meanings.shadow[0];
+    const love = tarot.tarot_interpretations[6].meanings.light[0];
+  
+
+    //display the information
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign (CENTER, CENTER);
+    text(fortune, width / 2, height /2); //description being the const you just created inside your function draw, it could also appear earlier in the code
+    pop();
+
+}
+
+
+function mousePressed (){
+    //choose a random card
+    const card = random(tarot.tarot_interpretations);
+    //choose a random fortune
+    fortune = random(card.fortune_telling)
 }

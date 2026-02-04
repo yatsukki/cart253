32e35f27cf6d6cbbf6177aa5c9801fd6dd19a5db
yatsukki <kervenc35@gmail.com>
2026-02-04 03:06:17 -0500

    practicing loops in arrays

---
+++ topic/loops/gradient/js/forvsof.js
@@ -0,0 +1,7 @@
+const fruits = ["apples", "peaches", "poop", "banana"]
+//here the i represents all of the array objects at the same time(its going through all of them)
+for (let i of fruits) {
+    console.log(i)
+}
+
+console.log(fruits[2])

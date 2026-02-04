6772ae0cac54c7c389881ba01c9382fae91b4cb1
yatsukki <kervenc35@gmail.com>
2026-02-03 21:01:23 -0500

    finished star field

--- topic/loops/template folder/js/script.js
+++ topic/loops/template folder/js/script.js
@@ -1,24 +1,46 @@
 /**
- * Title of Project
- * Author Name
+ * Star field
+ * Kerven
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws a star-field with a for loop
  */
 
 "use strict";
 
+const numStars = 100;
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(400, 400);
 }
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
-function draw() {
 
+//draw the star-field
+function draw() {
+background(0);
+
+    randomSeed(4);
+    for (let i = 0; i<numStars; i++) {
+        drawStar();
+    }
+
+}
+
+
+//draws random star
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    ellipse(x, y, diameter);
+    pop();
 }
---
+++ topic/loops/the-danger-zone/js/dangerwhile.js
@@ -0,0 +1,8 @@
+
+
+
+position the hero somwhere random
+
+while (the hero overlaps the danger zone) {
+    reposition the hero
+}
